1727d513bd7408222b8decad11c500c6ab4d6161
DJ <danieljohn@pop-os.localdomain>
2019-12-10 14:19:43 -0800

    pseudocoding caesar cipher

---
+++ section_one/caesar_cipher.rb
@@ -0,0 +1,17 @@
+# receive input as a string, character shift amount as num
+
+# encoded_string = ""
+
+# method shift_char(letter, shift)
+  # checks ASCII value of character
+  # if between a-z values, shifts left and wraps to z
+  # if between A-Z values, shifts left and wraps to Z
+  # if neither of above are true, returns letter unmodified
+  # returns new ASCII value
+
+# loop through input string
+  # temp.push(shift_char(input, shift_amount))
+
+# return encoded_string
+
+# https://www.rubyguides.com/2015/03/caesar-cipher-in-ruby/
